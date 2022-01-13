8aed26bfa86434bf3e76b3dc92d3b4470dd87fa8
rmq767 <2501706076@qq.com>
2022-01-13 21:54:00 +0800

    fix:文件名称大小写更改

---
+++ src/views/User/Registration/Index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <Department @change="changeDepartment" />
+    <DoctorTable />
+    <Pagination
+      :currentPage="pageInfo.currentPage"
+      :pageSize="pageInfo.pageSize"
+      :total="pageInfo.total"
+      @handleCurrentChange="handleCurrentChange"
+      @handleSizeChange="handleSizeChange"
+    ></Pagination>
+  </div>
+</template>
+
+<script lang="ts">
+import Pagination from "@/components/Pagination.vue";
+import { defineComponent, reactive, toRefs } from "vue";
+import Department from "./components/Department.vue";
+import DoctorTable from "./components/DoctorTable.vue";
+export default defineComponent({
+  name: "Registration",
+  components: {
+    Department,
+    DoctorTable,
+    Pagination,
+  },
+  setup() {
+    const state = reactive({
+      pageInfo: {
+        currentPage: 1,
+        pageSize: 10,
+        total: 0,
+      },
+    });
+
+    /**
+     * @description 选择科室
+     */
+    const changeDepartment = (value: string) => {
+      console.log(value);
+    };
+    /**
+     * @description 分页-每页条数
+     */
+    const handleSizeChange = (size: number) => {
+      console.log(size);
+    };
+    /**
+     * @description 分页-当前页数
+     */
+    const handleCurrentChange = (page: number) => {
+      console.log(page);
+    };
+    return {
+      ...toRefs(state),
+      changeDepartment,
+      handleSizeChange,
+      handleCurrentChange,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
